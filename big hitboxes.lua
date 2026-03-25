ae136336de42fb2d8cc7d98243930023b7e7e4f9
kaanaryka-code <kaanaryka2013@gmail.com>
2026-03-25 14:57:11 +0500

    Update big hitboxes.lua

--- big hitboxes.lua
+++ big hitboxes.lua
@@ -1,6 +1,6 @@
 -- Настройки
-local HITBOX_SIZE = 15 -- Размер хитбокса (стандарт 2, попробуй 15-20)
-local HITBOX_TRANSPARENCY = 0.7 -- Прозрачность (0.5 - видно, 1 - невидимо)
+local HITBOX_SIZE = 6
+local HITBOX_TRANSPARENCY = 0.8
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -8,11 +8,9 @@ local LP = Players.LocalPlayer
 
 RunService.RenderStepped:Connect(function()
     for _, player in pairs(Players:GetPlayers()) do
-        -- Проверяем, что это не мы и персонаж существует
         if player ~= LP and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
             local root = player.Character.HumanoidRootPart
             
-            -- Увеличиваем размер
             root.Size = Vector3.new(HITBOX_SIZE, HITBOX_SIZE, HITBOX_SIZE)
             
 
